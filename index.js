400a6041e21b65bdb6b08b4c950e84d77d268549
Grigorii Pashkov <gpashkov@my.centennialcollege.ca>
2022-11-10 20:00:42 -0500

    Final project

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const expresshbs = require("express-handlebars");
 const routeRoutes = require("./routes/rout");
+const path = require("path");
 const app = express();
 const hbs = expresshbs.create({
   layoutsDir: "views/layouts",
@@ -15,7 +15,10 @@ app.set("view engine", "hbs");
 app.set("views", "views");
 
 app.use(routeRoutes);
-
+app.use(express.static("Assignment1 301190058 Grigorii Pashkov"));
+app.use("/Images", express.static("Images"));
+app.use(express.static("Assignment1 301190058 Grigorii Pashkov"));
+app.use("/Files", express.static("Files"));
 app.listen(PORT, () => {
   console.log("Server +");
 });
--- routes/rout.js
+++ routes/rout.js
@@ -2,18 +2,28 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Home page",
+  });
 });
 router.get("/aboutMe", (req, res) => {
-  res.render("aboutMe");
+  res.render("aboutMe", {
+    title: "About Me",
+  });
 });
 router.get("/projects", (req, res) => {
-  res.render("projects");
+  res.render("projects", {
+    title: "My Projects",
+  });
 });
 router.get("/services", (req, res) => {
-  res.render("services");
+  res.render("services", {
+    title: "My Services",
+  });
 });
 router.get("/contacts", (req, res) => {
-  res.render("contacts");
+  res.render("contacts", {
+    title: "My Contacts",
+  });
 });
 module.exports = router;
